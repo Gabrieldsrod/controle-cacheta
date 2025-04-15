5b5e41a2eb3a94477f6dca361090c4d48eecd2cf
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2025-04-15 18:39:58 -0300

    Adiciona a classe Player com atributos e método, incluido getters e setters

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,5 +1,38 @@
 package entities;
 
 public class Player {
+    private String name;
+    private int totalTimeMinutes;
+    private double totalValueToPay;
 
+    public Player(String name) {
+        this.name = name;
+        this.totalTimeMinutes = 0;
+        this.totalValueToPay = 0.0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTotalTimeMinutes() {
+        return totalTimeMinutes;
+    }
+
+    public double getTotalValueToPay() {
+        return totalValueToPay;
+    }
+
+    public void addTime(int minutes) {
+        this.totalTimeMinutes += minutes;
+    }
+
+    public void addValue(double value) {
+        this.totalValueToPay += value;
+    }
+
+    public void reset() {
+        this.totalTimeMinutes = 0;
+        this.totalValueToPay = 0.0;
+    }
 }
