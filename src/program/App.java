5f6c344ae7ed18d62ad676226794cd85134d72a9
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2025-04-15 18:36:30 -0300

    Início projeto para controle de partidas de Cacheta

---
+++ src/program/App.java
@@ -0,0 +1,10 @@
+package program;
+
+
+public class App {
+    public static void main(String[] args) {
+        private List<Table> tableList = new ArrayList<>();
+        private double valurPerHour;
+
+    }
+}
