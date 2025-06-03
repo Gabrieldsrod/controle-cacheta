0f4ea50ff3cd352a0995ad3c7c44f1d0a8e09a21
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2025-06-03 13:22:15 -0300

    Adiciona geração de relatório diário do estabelecimento via console

--- Controle_Cacheta/src/service/ReportService.java
+++ Controle_Cacheta/src/service/ReportService.java
@@ -1,5 +1,30 @@
 package service;
 
+import entities.Game;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class ReportService {
-    /* TODO Gerar relatórios via json utilizando classe File */
+    public void estabilishmentReport(List<Game> games) {
+        LocalDate today = LocalDate.now();
+        int gamesPlayedToday = 0;
+        double raisedValueToday = 0.0;
+
+        System.out.println("========== RELATÓRIO DIÁRIO ==========");
+        for (Game g : games) {
+            if (g.getStartTime().toLocalDate().equals(today)) {
+                gamesPlayedToday++;
+                System.out.printf("Mesa %d: %s (%s) => R$%.2f\n",
+                        g.getTableNumber(),
+                        g.getFormattedTime(),
+                        g.getFormattedDuration(),
+                        g.getTotalValue());
+
+                raisedValueToday += g.getTotalValue();
+            }
+        }
+        System.out.println("Partidas jogadas hoje: " + gamesPlayedToday);
+        System.out.println("Valor total arrecado no hoje: R$" + String.format("%.2f", raisedValueToday));
+    }
 }
